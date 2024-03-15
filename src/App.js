d370d09418102c34c8ba4d09b4927ae63830c7d4
Azhar Ansari <azharansari@Azhars-MacBook-Air.local>
2024-03-15 23:40:24 +0530

    login/signup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Body from './components/Body';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
 
 
 function App() {
   return (
     <div >
-      <Body/>
+      <Provider store={appStore}> <Body/></Provider>
+     
     </div>
   );
 }
-
+ 
 export default App;
