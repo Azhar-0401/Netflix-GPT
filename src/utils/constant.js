869e35a889a407ea1a4232a67c25a8d83e9a3fae
Azhar Ansari <azharansari@Azhars-MacBook-Air.local>
2024-03-17 16:50:55 +0530

    all movies fetch

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+  export const API_OPTIONS =   {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzg3NzRkODE2ZjRjN2M1MjdhOGM1MTIxZDNlNzE0ZiIsInN1YiI6IjY1ZjZiOWZkZTE5NGIwMDE3Y2JlOWY3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UKSvooe3jcw7rl2jBWNpKxhO7xso-8o5dXtJp785beY'
+    }
+  };
+  
