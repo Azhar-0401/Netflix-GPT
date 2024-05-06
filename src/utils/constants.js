d479db6cc7f2459ba4abb0a47b30b60d07407cb3
Azhar-0401 <azharansari.surajpur@gmail.com>
2024-05-06 11:06:59 +0530

    tmdb

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,11 +5,11 @@ export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
 export const API_OPTIONS = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    accept: "application/json",
-    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
-  },
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzg3NzRkODE2ZjRjN2M1MjdhOGM1MTIxZDNlNzE0ZiIsInN1YiI6IjY1ZjZiOWZkZTE5NGIwMDE3Y2JlOWY3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UKSvooe3jcw7rl2jBWNpKxhO7xso-8o5dXtJp785beY'
+  }
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
