44da2ec5a71337465cf8a1647b31794da5615775
Azhar-0401 <azharansari.surajpur@gmail.com>
2024-05-06 12:19:22 +0530

    imdb

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -16,8 +16,11 @@ const VideoBackground = ({ movieId }) => {
           "?&autoplay=1&mute=1"
         }
         title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allow=" "
       ></iframe>
+ 
+
+{/* <iframe  className="w-screen aspect-video" src="https://www.youtube.com/embed/hXzcyx9V0xw?si=1_QNWAreSTkpovuC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
     </div>
   );
 };
